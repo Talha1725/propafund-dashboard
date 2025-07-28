56cc643aaed0f2b5c2d756e09b8af5a1c68e80d5
malir <maliramzan314@gmail.com>
2025-07-28 18:34:04 +0500

    Enhance Frame component to support 'none' variant and add hover effects in FundedSection. Introduce new coins SVG asset for visual elements.

--- components/home-page/funded-section.tsx
+++ components/home-page/funded-section.tsx
@@ -1,21 +1,88 @@
+"use client";
+
 import Image from "next/image";
 import Container from "../common/container";
-import priceStone1 from "@/public/assets/price-stones.svg"
-import priceStone2 from "@/public/assets/price-stone-2.svg"
+import Frame from "../common/frame";
+import { useState } from "react";
+import coins from "@/public/assets/coins.svg";
 
 export default function FundedSection() {
+  const [hover, setHover] = useState<string>("card2");
   return (
     <div className="font-creato-display py-30">
       <Container>
         <h1 className="text-center font-romanica uppercase text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold lg:font-normal">
           From Citizen to Funded Hero
         </h1>
-        <p className="text-center mt-3">Our streamlined process transforms skilled traders into funded warriors.</p>
+        <p className="text-center mt-3">
+          Our streamlined process transforms skilled traders into funded
+          warriors.
+        </p>
 
-        <div className="grid grid-cols-3 gap-14">
-          <div className="p-4 border border-white/10 gradient-dark">
-          <Image src={priceStone1} alt="price-stone" className="w-full h-full bg-no-repeat object-cover" />
-          </div>
+        <div className="grid grid-cols-3 gap-14 mt-15">
+          <Frame variants={hover === "card1" ? "white" : "none"}>
+            <div
+              onMouseEnter={() => setHover("card1")}
+              onMouseLeave={() => setHover("")}
+              className={`p-4 border border-white/10 h-[313px] flex flex-col justify-end items-center transition-all duration-300 ease-in-out ${
+                hover === "card1" ? "gradient-primary" : "gradient-dark"
+              }`}
+            >
+                            <Image src={coins} alt="price-stone" className="w-full h-full bg-no-repeat object-cover" />
+
+              <h1 className="font-romanica text-xl uppercase">
+                Choose Your Challenge
+              </h1>
+              <p
+                className={`text-center mt-2 md:w-[70%] mx-auto ${
+                  hover === "card1" ? "text-white" : "text-white/30"
+                }`}
+              >
+                Pick your funding level and start your journey.
+              </p>
+            </div>
+          </Frame>
+          <Frame variants={hover === "card2" ? "white" : "none"}>
+            <div
+              onMouseEnter={() => setHover("card2")}
+              onMouseLeave={() => setHover("")}
+              className={`p-4 border border-white/10 h-[313px] flex flex-col justify-end items-center transition-all duration-300 ease-in-out ${
+                hover === "card2" ? "gradient-primary" : "gradient-dark"
+              }`}
+            >
+                            <Image src={coins} alt="price-stone" className="w-full h-full bg-no-repeat object-cover" />
+
+              <h1 className="font-romanica text-xl uppercase">
+                Prove Your Discipline{" "}
+              </h1>
+              <p
+                className={`text-center mt-2 md:w-[70%] mx-auto ${
+                  hover === "card1" ? "text-white" : "text-white/30"
+                }`}
+              >
+                Trade responsibly, hit targets, and follow the rules.{" "}
+              </p>
+            </div>
+          </Frame>
+          <Frame variants={hover === "card3" ? "white" : "none"}>
+            <div
+              onMouseEnter={() => setHover("card3")}
+              onMouseLeave={() => setHover("")}
+              className={`p-4 border border-white/10 h-[313px] flex flex-col justify-end items-center transition-all duration-300 ease-in-out ${
+                hover === "card3" ? "gradient-primary" : "gradient-dark"
+              }`}
+            >
+              <Image src={coins} alt="price-stone" className="w-full h-full bg-no-repeat object-cover" />
+              <h1 className="font-romanica text-xl uppercase">Get Funded </h1>
+              <p
+                className={`text-center mt-2 md:w-[70%] mx-auto ${
+                  hover === "card1" ? "text-white" : "text-white/30"
+                }`}
+              >
+                Receive capital and earn real profits with zero risk.{" "}
+              </p>
+            </div>
+          </Frame>
         </div>
       </Container>
     </div>
