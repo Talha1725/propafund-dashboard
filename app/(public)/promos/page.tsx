b81d1ceb570fc43f91afe2066c0ca3d914873fe1
Saba Siddique <sabasiddique001@gmail.com>
2025-09-25 23:02:36 +0500

    feat: promo page implementation

--- app/(public)/promos/page.tsx
+++ app/(public)/promos/page.tsx
@@ -1,6 +1,10 @@
 import Container from "@/components/common/container";
 import HeroSection from "@/components/home-page/hero-section";
 import promoBg from "@/public/assets/promo-bg.svg";
+import OracleSection from "@/components/home-page/oracle-section";
+import Glow from "@/components/common/glow";
+import Image from "next/image";
+import hammer from "@/public/assets/marble-hammer.svg";
 
 export default function FAQPage() {
   return (
@@ -13,8 +17,15 @@ export default function FAQPage() {
         showButton={false}
         isHomepage={false}
       />
-      <div className="font-creato-display py-30">
+      <div className="font-creato-display" id="promos">
         <Container>
+          <OracleSection className="-py-30"/>
+          <div className="relative">
+          <div className="pointer-events-none absolute inset-x-0 -top-150 flex justify-center">
+            <Glow width={2600} height={1800} opacity={0.45} shape="farthest-side" blur={100} />
+          </div>
+          <Image src={hammer} alt="hammer" className="w-full h-full object-cover" />
+        </div>
         </Container>
       </div>
     </>
