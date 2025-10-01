cfe42f242600b8c4bf4114b65c415e9ad4f71a63
Saba Siddique <sabasiddique001@gmail.com>
2025-10-01 14:50:07 +0500

    feat: add space

--- app/(dashboard)/user/academy/page.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardPageContainer from "@/components/common/dashboard-page-container";
-
-export default function AcademyPage() {
-  return (
-    <DashboardPageContainer>
-      <div>Academy</div>
-    </DashboardPageContainer>
-  );
-}
