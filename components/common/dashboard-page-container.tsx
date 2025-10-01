aea9700681c26bb6e65861ecafcd1204d58455b4
malir2 <maliramzan314@gmail.com>
2025-10-01 14:04:25 +0500

    Update dashboard page container styles to adjust height and add outline

--- components/common/dashboard-page-container.tsx
+++ components/common/dashboard-page-container.tsx
@@ -1,6 +1,6 @@
 export default function DashboardPageContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="p-3 md:p-6 md:pb-4 space-y-5 xl:h-[85vh] overflow-auto relative">
+    <div className="p-3 md:p-6 md:pb-4 space-y-5 xl:h-[86.1vh] overflow-auto relative outline-0">
       <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full h-[200px] bg-gradient-to-b from-blue to-blue/50 rotate-[14deg] blur-3xl opacity-20 z-0 pointer-events-none"></div>
       {children}
     </div>
